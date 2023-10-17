5414d9a3f4bc596b9ee0576102d59da9e82483c9
mn <mn.de@outlookl.com>
2023-10-17 15:19:49 +0200

    gamecharacters

--- src/gamecharacters/Game.java
+++ src/gamecharacters/Game.java
@@ -1,9 +1,11 @@
 package gamecharacters;
 
+// Define the Character interface with a method useAbility
 interface Character {
     String useAbility();
 }
 
+// Create the Warrior class that implements the Character interface
 class Warrior implements Character {
     @Override
     public String useAbility() {
@@ -11,30 +13,34 @@ class Warrior implements Character {
     }
 }
 
+// Create the Mage class that implements the Character interface
 class Mage implements Character {
     @Override
     public String useAbility() {
-        return "The Mega conjures storm of ice and fros!t";
+        return "The Mage conjures a storm of ice and frost!";
     }
 }
 
+// Create the Archer class that implements the Character interface
 class Archer implements Character {
     @Override
     public String useAbility() {
         return "The Archer unleashes a barrage of arrows with unmatched precision!";
     }
 }
-
 public class Game {
     public static void main(String[] args) {
-       
+        // Create an array of characters with different classes
         Character[] characters = {new Warrior(), new Mage(), new Archer()};
-       
-        for (Character chara : characters) {
-            System.out.println(chara.useAbility());
+        
+        // Loop through the array and display each character's ability
+        for (Character character : characters) {
+            System.out.println(character.useAbility());
         }
     }
 }
+
+
 
 
 
